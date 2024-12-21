415db30e7403a00f224350b829ef16764bed17f5
caster9 <149708808+caster9@users.noreply.github.com>
2024-12-21 02:00:39 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+// Function to show the diagram when the button is clicked
+function showDiagram() {
+    var diagram = document.getElementById("diagram");
+    if (diagram.style.display === "none") {
+        diagram.style.display = "block";
+    } else {
+        diagram.style.display = "none";
+    }
+}
